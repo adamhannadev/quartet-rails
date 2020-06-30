9b372c05ae76c8dbdb5a697c2b191cae5879a190
Adam Hanna <adamhanandev@gmail.com>
2020-06-29 17:43:23 -0700

    Remove home controller and generate main controller

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def dashboard
+  end
+
+  def choose-song
+  end
+
+  def record
+  end
+
+  def mix
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'main/index'
+  get 'main/dashboard'
+  get 'main/choose-song'
+  get 'main/record'
+  get 'main/mix'
   devise_for :users
-  root to: "home#index"
+  root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

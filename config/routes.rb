12644dea2c68999ca3d9f8cb4c304399c361138d
Adam Hanna <adamhanandev@gmail.com>
2020-07-10 14:15:28 -0700

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :recordings
-  get 'dashboard' => 'main#dashboard'
-  get 'choose-song'=> 'main#choose-song'
-  get 'record' => 'main#record'
-  get 'mix' => 'main#mix'
   devise_for :users
   root "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

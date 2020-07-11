885dd9be046ba813cf88557211f9fac34c89af5e
Adam Hanna <adamhanandev@gmail.com>
2020-07-11 15:45:32 -0700

    Allow the recording form to record audio from the browser and play it back

--- app/javascript/controllers/recording_controller.js
+++ app/javascript/controllers/recording_controller.js
@@ -9,10 +9,64 @@
 
 import { Controller } from "stimulus"
 
+const audioType = "audio/wav";
+
 export default class extends Controller {
-  static targets = ["output"]
+  static targets = ["activeTrack", "saved", "recFile"]
 
   connect() {
     console.log("Hello from the recording controller.")
+    this.state = {
+      audios: []
+    }
+    this.initRecording();
   }
+
+  async initRecording() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    this.activeTrackTarget.srcObject = stream;
+    // init recording
+    this.mediaRecorder = new MediaRecorder(stream);
+    // init data storage for audio chunks
+    this.chunks = [];
+    // this.bgAudio = new Audio(`InTheBulb.mp3`);
+    // this.bgAudio.load();
+    // listen for data from media recorder
+    this.mediaRecorder.ondataavailable = (e) => {
+      if (e.data && e.data.size > 0) {
+        this.chunks.push(e.data);
+      }
+    };
+  }
+
+
+  start(e) {
+    e.preventDefault();
+    // this.bgAudio.play();
+    // wipe old data chunks
+    this.chunks = [];
+    // start recorder with 10ms buffer
+    this.mediaRecorder.start(10);
+  }
+
+  stop(e) {
+    e.preventDefault();
+    // this.bgAudio.pause();
+    // this.bgAudio.currentTime = 0;
+    // stop the recorder
+    this.mediaRecorder.stop();
+    // say that we're not recording
+    this.saveAudio();
+  }
+
+  saveAudio() {
+    // convert saved chunks to blob
+    const blob = new Blob(this.chunks, { type: audioType });
+    // generate audio url from blob
+    const audioURL = window.URL.createObjectURL(blob);
+    this.savedTarget.src = audioURL;
+    this.recFileTarget.value = blob;
+  }
+
 }
+
